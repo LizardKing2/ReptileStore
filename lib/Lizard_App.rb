5b0f666c9c8ba0d00551062ad369f38c60714e84
LizardKing2 <elizabethking444@gmail.com>
2017-03-28 10:41:01 +1100

    C1.2.1 - JSON read in
    * Fix to JSON format
    * Added JSON parse in and using reptile_type as a key

--- lib/Lizard_App.rb
+++ lib/Lizard_App.rb
@@ -2,5 +2,8 @@
 require 'JSON'
 class Lizard_App
   def self.call(cost_json)
+
+      datacost = JSON.parse(cost_json)
+      puts datacost.collect {|reptile_type| reptile_type.fetch('reptile_type')}
   end
 end
