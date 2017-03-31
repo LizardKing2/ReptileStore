439450af177528bb6f02cca08df9c2015e9e01d6
LizardKing2 <elizabethking444@gmail.com>
2017-03-31 12:36:58 +1100

    C1.2 - Customers added
    # Customer JSON added
    # Customer call added
    # Customer name key returned

--- Rakefile
+++ Rakefile
@@ -7,9 +7,10 @@ task "run" do
   $LOAD_PATH.unshift(File.dirname(__FILE__), "lib")
   require 'Lizard_App'
 
-  cost_json = File.read("data/cost.json")
+ cost_json = File.read("data/cost.json")
+  customer_json = File.read("data/customer.json")
 
   # call the Lizard-- I should change this if I want to do more reptiles..hmm
-  result_json = Lizard_App.call(cost_json)
-
+ result_json = Lizard_App.call(cost_json)
+  resultcustomer_json = Lizard_App.call(customer_json)
 end
--- lib/Lizard_App.rb
+++ lib/Lizard_App.rb
@@ -1,10 +1,13 @@
 
 require 'JSON'
 class Lizard_App
+  def self.call(customer_json)
+      datacost = JSON.parse(customer_json)
+      puts datacost.collect {|name| name.fetch('name')}
+  end
   def self.call(cost_json)
-
-      datacost = JSON.parse(cost_json)
-      puts datacost.collect {|reptile_type| reptile_type.fetch('reptile_type')}
-      puts datacost.collect{|cost| cost.fetch('cost')}
+    datacost = JSON.parse(cost_json)
+    puts datacost.collect {|reptile_type| reptile_type.fetch('reptile_type')}
+    #puts datacost.collect{|cost| cost.fetch('cost')}
   end
 end
